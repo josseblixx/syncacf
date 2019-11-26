d1844ea11347cdd10461ab06d3f394e24d637d6d
Josse <josse@blixx.nl>
2019-11-26 15:01:09 +0100

    First version

---
+++ src/BlixxSyncACF/SyncACF.php
@@ -0,0 +1,62 @@
+<?php
+
+    namespace BlixxSyncACF;
+
+    class SyncACF
+    {
+
+        static private $_instance;
+
+        /**
+         *
+         */
+        static public function sync()
+        {
+
+            self::instance()->hook();
+
+        }
+
+
+        /**
+         * @return SyncACF
+         */
+        static public function instance()
+        {
+
+            return self::$_instance ?: new self();
+
+        }
+
+
+        private function hook()
+        {
+
+            add_filter('acf/settings/save_json', function () {
+
+                // update path
+                $path = get_stylesheet_directory() . '/advanced-custom-fields';
+
+                if (!file_exists($path)){
+                    if (!(mkdir($path) && is_dir($path))){
+                        throw new \Exception('Cannot create sync folder for ACF');
+                    }
+                }
+
+                // return
+                return $path;
+
+            });
+
+            add_filter('acf/settings/load_json', function( ) {
+
+                // append path
+                return [get_stylesheet_directory() . '/advanced-custom-fields'];
+
+            });
+
+        }
+
+
+    }
+
